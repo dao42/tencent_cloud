864633133ceca528706660821f0e64f37c10cd02
weilong <chenweilong@yeezon.com>
2022-04-21 14:35:24 +0800

    refactor: update  get_federation_token policy

--- lib/tencent_cloud/request.rb
+++ lib/tencent_cloud/request.rb
@@ -5,6 +5,28 @@ require 'erb'
 module TencentCloud
   module Request
     def get_federation_token
+      policy = {
+        "version": "2.0",
+        "statement": [
+          {
+            "action": [
+              "name/cos:GetObject",
+              # 简单上传
+              "name/cos:PutObject",
+              # 表单上传、小程序上传
+              "name/cos:PostObject",
+              # 分块上传
+              "name/cos:InitiateMultipartUpload",
+              "name/cos:ListMultipartUploads",
+              "name/cos:ListParts",
+              "name/cos:UploadPart",
+              "name/cos:CompleteMultipartUpload"
+            ],
+            "resource": "qcs::cos:#{config.region}/#{config.secret_id}:#{config.bucket}/*",
+            "effect": "allow"
+          }
+        ]
+      }.to_json
       payload = {
         service: 'sts',
         headers: {
@@ -13,23 +35,7 @@ module TencentCloud
           'X-TC-Version' => '2018-08-13'
         },
         'Name' => 'smb',
-        'Policy' => ERB::Util.url_encode({
-          "version": "2.0",
-          "statement": [
-            {
-              "action": [
-                "name/cos:*"
-              ],
-              "resource": "*",
-              "effect": "allow"
-            },
-            {
-              "effect": "allow",
-              "action": "monitor:*",
-              "resource": "*"
-            }
-          ]
-        }.to_json)
+        'Policy' => ERB::Util.url_encode(policy)
       }
       endpoint = "https://#{payload.dig(:headers, 'host')}"
       make_get(endpoint, payload)
